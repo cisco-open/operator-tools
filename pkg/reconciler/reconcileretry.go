0a24290b0b1061c4460bd43ca90e2119079a4603
pepov <peterwilcsinszky@gmail.com>
2020-02-10 17:00:59 +0100

    reconcile retry

---
+++ pkg/reconciler/reconcileretry.go
@@ -0,0 +1,36 @@
+package reconciler
+
+import (
+	"time"
+
+	"emperror.dev/errors"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type ReconcileRetry struct {
+	MaxRetries  int
+	DefaultWait time.Duration
+}
+
+func (r *ReconcileRetry) Reconcile(component func() (*reconcile.Result, error)) error {
+	i := 0
+	for ; i < r.MaxRetries || r.MaxRetries == -1; i++ {
+		result, err := component()
+		if err != nil {
+			return err
+		}
+		if result == nil {
+			return nil
+		}
+		if result.Requeue == false && result.RequeueAfter == 0 {
+			break
+		} else {
+			if result.RequeueAfter > 0 {
+				time.Sleep(result.RequeueAfter)
+			} else {
+				time.Sleep(r.DefaultWait)
+			}
+		}
+	}
+	return errors.Errorf("reconciliation did not complete after %d retries", i)
+}
