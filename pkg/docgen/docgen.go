47abcf680642807cf74581e2cb7fdda40a31d368
Robert Fekete <fekete77.robert@gmail.com>
2021-05-04 16:23:08 +0200

    docgen.go: fixes linebreaks

--- pkg/docgen/docgen.go
+++ pkg/docgen/docgen.go
@@ -142,7 +142,7 @@ func (d *Doc) visitNode(n ast.Node) bool {
 			structure, ok := typeName.Type.(*ast.StructType)
 			if ok && typeName.Name.IsExported() {
 				d.Append(fmt.Sprintf("## %s", getTypeName(generic, typeName.Name.Name)))
-				d.Append("\n")
+				d.Append("") // Adds a line-break for markdown formatting
 				if getTypeDocs(generic, true) != "" {
 					d.Append(fmt.Sprintf("%s", getTypeDocs(generic, true)))
 				}
@@ -155,7 +155,7 @@ func (d *Doc) visitNode(n ast.Node) bool {
 					}
 					d.Append(fmt.Sprintf("| %s | %s | %s | %s | %s |", name, d.normaliseType(item.Type), required, def, com))
 				}
-				d.Append("\n")
+				d.Append("") // Adds a line-break for markdown formatting
 			}
 		}
 	}
